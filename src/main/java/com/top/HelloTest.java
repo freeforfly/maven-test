ad9afe03a5b7d62d085e043626f155846def89f2
freeforfly <178525659@.qqcom>
2018-07-07 09:36:27 +0800

    init

---
+++ src/main/java/com/top/HelloTest.java
@@ -0,0 +1,13 @@
+package com.top;
+
+/**
+ * Description
+ * @author 袁幸成【yuanxc@3vjia.com】
+ * @since 2018/7/7 09:32
+ */
+public class HelloTest {
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
